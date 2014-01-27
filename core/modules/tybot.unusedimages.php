01f6f3e8f283e82760f243dbf6363d8f83f3f528
DoctorWhooves <cataclysmicpinkiepie64@gmail.com>
2014-01-27 02:20:27 +0000

    fix

--- core/modules/tybot.unusedimages.php
+++ core/modules/tybot.unusedimages.php
@@ -32,18 +32,18 @@ class UnusedImages extends Tybot {
         foreach($result["query"]["querypage"]["results"] as $image) {
         
             #Print message
-            print("Attempting to delete File:" . $result["title"] . "\n");
+            print("Attempting to delete " . $image["title"] . "\n");
             
             #Delete image
             $result = $this->deletePage($image["title"], "Unused image - bot");
             
             if (empty($result["error"])) {
             
-                print("File:" . $image["title"] . " deleted!\n");
+                print($image["title"] . " deleted!\n");
                 
             } else {
             
-                print("Failed to delete File:" . $image["title"] . "\n");
+                print("Failed to delete " . $image["title"] . "\n");
                 
             }
             
